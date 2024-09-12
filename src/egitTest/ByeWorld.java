392059677f04f6f04dbde35191a5926e2b1fe718
chahyoil <chahyoil@gmail.com>
2024-09-12 17:05:08 +0900

    init

---
+++ src/egitTest/ByeWorld.java
@@ -0,0 +1,8 @@
+package egitTest;
+
+public class ByeWorld {
+	public static void main(String[] args) {
+		//ToDo Auto-generated method stub
+		System.out.println("Bye World");
+	}
+}
---
+++ src/egitTest/HelloWorld.java
@@ -0,0 +1,9 @@
+package egitTest;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		//ToDo Auto-generated method stub
+		System.out.println("Hello World");
+	}
+}
